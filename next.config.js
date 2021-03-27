0b576030808133afbfb642118870a2c0a4253274
lazaroroox <lazarobransford@hotmail.com>
2021-03-27 17:34:55 -0300

    1

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ module.exports = {
     env: {
       'MYSQL_HOST': '31.220.104.197',
       'MYSQL_PORT': '3306',
-      'MYSQL_DATABASE': {database_name},
-      'MYSQL_USER': {user_name},
-      'MYSQL_PASSWORD': {user_password},
+      'MYSQL_DATABASE': 'u786197823_novel',
+      'MYSQL_USER': 'u786197823_novel',
+      'MYSQL_PASSWORD': '|ODMgqDn1:Sh',
     }
   }
   
