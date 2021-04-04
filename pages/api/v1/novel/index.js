d2360324aa39645a4f954294a006000df114bd49
lazaroroox <lazarobransford@hotmail.com>
2021-04-04 00:35:27 -0300

    main

--- pages/api/v1/novel/index.js
+++ pages/api/v1/novel/index.js
@@ -1,4 +1,4 @@
-import { adicionarNovel, verNovel, editarNovel } from '../../../../lib/novel';
+import { adicionarNovel, verNovel, editarNovel, novelItem, todosNovel } from '../../../../lib/novel';
 
 export default async (req, res) => {
 
@@ -28,6 +28,22 @@ export default async (req, res) => {
                 msg: 'Não foi possivel salvar.'
             })
         }
+    } else if (req.method === "GET") {
+        const novel = await todosNovel();
+
+
+        const resultado = [];
+
+        for (const final of novel) {
+            const ff = await novelItem(final);
+            resultado.push(ff);
+        }
+
+        res.status(200).json({
+            status: 'sucesso',
+            data: resultado
+        });
+
     } else {
         res.status(203).json({
             status: 'erro',
